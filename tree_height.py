85ac9e8e08b5db028a76ca98156ae534283bef92
vladislavantoniv <125074315+vladislavantoniv@users.noreply.github.com>
2023-03-05 11:16:46 +0000

    1

--- tree_height.py
+++ tree_height.py
@@ -2,16 +2,56 @@
 
 import sys
 import threading
+import numpy as np
 
 
 def compute_height(n, parents):
     # Write this function
-    max_height = 0
+    print("numcount=",n)
+    print("arr",parents)
+    max_height=0;
+    for i in range (n):
+        n=i
+        count=0
+        while n!=-1:
+            n=parents[n]
+            count=count+1
+        if max_height < count:
+            max_height=count
     # Your code here
     return max_height
 
 
 def main():
+    choose=input()
+    choose_arr=[]
+    parents=[]
+    for i,next in enumerate(choose):
+        choose_arr.append(next)
+    if choose_arr[0]=="I":
+        numcount=input()
+        numcount=int(numcount)
+        print(type(numcount))
+        print(numcount)
+        text=input()
+        print(text)
+        parents=np.fromstring(text, dtype=int, sep=' ')
+        print(parents)
+        print(type(parents))
+        print("result=")
+        print(compute_height(numcount,parents))
+    elif choose_arr[0]=="F":
+        fileName=input()
+        if fileName[0]!='a':
+            fileName="/workspaces/tree-height-from-empty-vladislavantoniv/test/"+fileName+".txt"
+            with open(fileName,"r") as f:
+                numcount=f.readlines()[0]
+                numcount=int(numcount)
+            with open(fileName,"r") as f:
+                text=f.readlines()[1]
+            parents=np.fromstring(text, dtype=int, sep=' ')
+            print(compute_height(numcount,parents))
+        
     # implement input form keyboard and from files
     
     # let user input file name to use, don't allow file names with letter a
@@ -20,11 +60,13 @@ def main():
     # input number of elements
     # input values in one variable, separate with space, split these values in an array
     # call the function and output it's result
-
+    pass
 
 # In Python, the default limit on recursion depth is rather low,
 # so raise it here for this problem. Note that to take advantage
 # of bigger stack, we have to launch the computation in a new thread.
-sys.setrecursionlimit(10**7)  # max depth of recursion
-threading.stack_size(2**27)   # new thread will get stack of such size
-threading.Thread(target=main).start()
+#sys.setrecursionlimit(10**7)  # max depth of recursion
+#threading.stack_size(2**27)   # new thread will get stack of such size
+#threading.Thread(target=main).start()
+main()
+# print(numpy.array([1,2,3]))
